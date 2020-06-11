80128c41619d003aec047bf49a407cdf97a9f023
LiliwoL <github@liliwol.fr>
2020-06-11 14:00:22 +0200

    Asset script

--- public/js/script.js
+++
@@ -1,8 +0,0 @@
-
-
-// Création de la carte
-var map = L.map('mapid').setView([ {{ resultat.coord.lat }},{{ resultat.coord.lon }}], 10);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
